6bc04dfdefe298e5c861c396522674b5ed092f86
highlander <bithighlander@gmail.com>
2023-03-05 00:08:05 -0700

    footer button

--- frontend/src/lib/layout/Footer.tsx
+++ frontend/src/lib/layout/Footer.tsx
@@ -5,43 +5,15 @@ import axios from "axios";
 
 const Footer = () => {
 
-  const clearSession = async function () {
-    try {
-      // eslint-disable-next-line no-console
-      console.log("onDone: ");
-
-      //fullfill
-      const body = {
-        sessionId:"test"
-      };
-      let submitResp = await axios.post(
-          "http://127.0.0.1:4000/api/v1/clear",
-          body
-      );
-      submitResp = submitResp.data
-      // eslint-disable-next-line no-console
-      console.log("submitResp: ", submitResp);
-      window.location.reload();
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error(e);
-    }
-  };
-
-
   return (
     <Flex
       as="footer"
       width="full"
       align="center"
-      alignSelf="flex-end"
       justifyContent="center"
     >
       <Text fontSize="xl">
         <Rate />
-        {/*<br/>*/}
-        {/*<Session />*/}
-        <Button onClick={clearSession}>end session</Button>
       </Text>
     </Flex>
   );
--- frontend/src/lib/layout/Rate.tsx
+++ frontend/src/lib/layout/Rate.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@chakra-ui/react";
+import { Grid, Button } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect } from "react";
 
@@ -7,6 +7,30 @@ const Onramp = () => {
     rate: "...",
     sessionId: "...",
   });
+
+  const clearSession = async function () {
+    try {
+      // eslint-disable-next-line no-console
+      console.log("onDone: ");
+
+      //fullfill
+      const body = {
+        sessionId:"test"
+      };
+      let submitResp = await axios.post(
+          "http://127.0.0.1:4000/api/v1/clear",
+          body
+      );
+      submitResp = submitResp.data
+      // eslint-disable-next-line no-console
+      console.log("submitResp: ", submitResp);
+      window.location.reload();
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e);
+    }
+  };
+
 
   const onStart = async function () {
     try {
@@ -30,7 +54,7 @@ const Onramp = () => {
 
   return (
     <Grid textAlign="center" gap={2}>
-      <h2>{Number(status.rate).toFixed(2)} LUSD/USD | {(1 / Number(status.rate)).toFixed(2)} USD/LUSD</h2>
+      <h2>{Number(status.rate).toFixed(2)} LUSD/USD | <Button colorScheme='teal' onClick={clearSession}>end session</Button> | {(1 / Number(status.rate)).toFixed(2)} USD/LUSD</h2>
     </Grid>
   );
 };
