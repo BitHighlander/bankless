74a35c6dd9ba3b1db0bbdceba030e3b3bd529e45
highlander <bithighlander@gmail.com>
2023-03-05 12:09:39 -0700

    remove slider unfinished

--- frontend/src/lib/pages/home/components/Onramp.tsx
+++ frontend/src/lib/pages/home/components/Onramp.tsx
@@ -259,39 +259,39 @@ const Onramp = () => {
             >
               Payout Crypto
             </Button></div>):(<div>
-              <Slider
-                  id='slider'
-                  defaultValue={5}
-                  min={0}
-                  max={100}
-                  colorScheme='teal'
-                  onChange={(v) => handleQuote(v)}
-                  onMouseEnter={() => setShowTooltip(true)}
-                  onMouseLeave={() => setShowTooltip(false)}
-              >
-                <SliderMark value={25} mt='1' ml='-2.5' fontSize='sm'>
-                  25%
-                </SliderMark>
-                <SliderMark value={50} mt='1' ml='-2.5' fontSize='sm'>
-                  50%
-                </SliderMark>
-                <SliderMark value={75} mt='1' ml='-2.5' fontSize='sm'>
-                  75%
-                </SliderMark>
-                <SliderTrack>
-                  <SliderFilledTrack />
-                </SliderTrack>
-                <Tooltip
-                    hasArrow
-                    bg='teal.500'
-                    color='white'
-                    placement='top'
-                    isOpen={showTooltip}
-                    label={`${sliderValue}%`}
-                >
-                  <SliderThumb />
-                </Tooltip>
-              </Slider>
+              {/*<Slider*/}
+              {/*    id='slider'*/}
+              {/*    defaultValue={5}*/}
+              {/*    min={0}*/}
+              {/*    max={100}*/}
+              {/*    colorScheme='teal'*/}
+              {/*    onChange={(v) => handleQuote(v)}*/}
+              {/*    onMouseEnter={() => setShowTooltip(true)}*/}
+              {/*    onMouseLeave={() => setShowTooltip(false)}*/}
+              {/*>*/}
+              {/*  <SliderMark value={25} mt='1' ml='-2.5' fontSize='sm'>*/}
+              {/*    25%*/}
+              {/*  </SliderMark>*/}
+              {/*  <SliderMark value={50} mt='1' ml='-2.5' fontSize='sm'>*/}
+              {/*    50%*/}
+              {/*  </SliderMark>*/}
+              {/*  <SliderMark value={75} mt='1' ml='-2.5' fontSize='sm'>*/}
+              {/*    75%*/}
+              {/*  </SliderMark>*/}
+              {/*  <SliderTrack>*/}
+              {/*    <SliderFilledTrack />*/}
+              {/*  </SliderTrack>*/}
+              {/*  <Tooltip*/}
+              {/*      hasArrow*/}
+              {/*      bg='teal.500'*/}
+              {/*      color='white'*/}
+              {/*      placement='top'*/}
+              {/*      isOpen={showTooltip}*/}
+              {/*      label={`${sliderValue}%`}*/}
+              {/*  >*/}
+              {/*    <SliderThumb />*/}
+              {/*  </Tooltip>*/}
+              {/*</Slider>*/}
               (deposit cash....)</div>)}
           </div>)}
         </div>
