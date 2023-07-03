eef9fa3cc4abca1a633ecf7deab461436e09d81c
battlejoose <battlejoose@gmail.com>
2023-07-02 18:55:33 -0600

    lowercase contract

--- backend/__tests__/test-module.js
+++ backend/__tests__/test-module.js
@@ -11,4 +11,35 @@
 
  */
 
+/*
+import axios from 'axios';
+ */
+
+let axios = require('axios')
+let assert = require('assert');
+
+let run_test = async () => {
+    try{
+
+        //hit fake endpoint
+        const bodyFund = {
+            amount:"10"
+        };
+        console.log("bodyFund: ",bodyFund)
+        let respFund = await axios.post(
+            "http://127.0.0.1:4000/api/v1/hack/withdrawalCash",
+            bodyFund
+        );
+        respFund = respFund.data
+        // eslint-disable-next-line no-console
+        console.log("respFund: ", respFund);
+
+
+
+    }catch(e){
+        console.error(e)
+    }
+}
+run_test()
+
 
