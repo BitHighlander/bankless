f500aa9b638408669c76916970835f3f98cead30
bithighlander <bithighlander@gmail.com>
2023-07-20 17:04:34 -0600

    LP add working

--- backend/src-api/capTable.ts
+++ backend/src-api/capTable.ts
@@ -1,4 +1,3 @@
-const TAG = " | Bankless-Backend | ";
 import axios from 'axios';
 const SspLib = require('@keepkey/encrypted-smiley-secure-protocol');
 const uuid = require('short-uuid');
@@ -10,252 +9,219 @@ const database = require('./database');
 let wait = require('wait-promise');
 let sleep = wait.sleep;
 
+const TAG = " | Bankless-Backend | ";
 let PROTOCOL_1PCT = "0x651982e85D5E43db682cD6153488083e1b810798";
 
 let ATM_OWNER = process.env['ATM_OWNER'];
 if (!ATM_OWNER) throw Error("Invalid ENV missing ATM_OWNER");
 
-// TABLE OWNER
-let CAP_TABLE = [];
+interface CapEntry {
+    id: number;
+    address: string;
+    lpTokens: number;
+    percentage: number;
+}
+
+let CAP_TABLE: CapEntry[] = [];
 let TOTAL_USD = 0;
 let TOTAL_DAI = 0;
 let TOTAL_LP_TOKENS = 0;
 
-module.exports = {
-    init: async function () {
-        return init_cap();
-    },
-    get: async function () {
-        return CAP_TABLE;
-    },
-    sync: async function (usd, dai) {
-        TOTAL_USD = usd;
-        TOTAL_DAI = dai;
-        return true;
-    },
-    getByOwner: async function () {
-        return CAP_TABLE;
-    },
-    valueByOwner: async function () {
-        return calculateValueByOwner();
-    },
-    add: async function (address, amountUSD, amountDAI) {
-        return add_cap(address, amountUSD, amountDAI);
-    },
-    remove: async function (address, percent) {
-        return remove_cap(address, percent);
+export async function init(): Promise<void> {
+    try {
+        await init_cap();
+    } catch (error) {
+        log.info(TAG + 'init_cap error:', error);
+        throw error;
     }
-};
-
-function calculateValueByOwner() {
-    const valueByOwner = [];
-    const totalLPTokens = getTotalLPTokens();
-
-    for (const entry of CAP_TABLE) {
-        const address = entry.address;
-        const lpTokens = entry.lpTokens;
-
-        const valueUSD = (lpTokens / totalLPTokens) * TOTAL_USD;
-        const valueDAI = (lpTokens / totalLPTokens) * TOTAL_DAI;
-
-        valueByOwner.push({
-            address: address,
-            valueUSD: valueUSD,
-            valueDAI: valueDAI,
-        });
-    }
-
-    return valueByOwner;
 }
 
-/*
-    If no cap entities in DB then we init DB
+export function get(): CapEntry[] {
+    return CAP_TABLE;
+}
 
-    Assign ALL assets to owner of ATM
+export function sync(usd: number, dai: number): void {
+    TOTAL_USD = usd;
+    TOTAL_DAI = dai;
+}
 
-    @TODO autit every startup and verify USD of pool prior to shutdown
-    LP owners need to know if owner has removed assets without receipt!
- */
+export function getByOwner(): CapEntry[] {
+    return CAP_TABLE.filter(entry => entry.address === ATM_OWNER);
+}
 
-let init_cap = async function () {
-    let tag = TAG + " | init_cap | ";
+export function valueByOwner(): { address: string; valueUSD: number; valueDAI: number }[] {
+    return calculateValueByOwner();
+}
+
+export function add(address: string, amountUSD: number, amountDAI: number): Promise<CapEntry> {
     try {
-        log.info("Initing Cap table!")
-        log.info("TOTAL_DAI: ", TOTAL_DAI)
-        log.info("TOTAL_USD: ", TOTAL_USD)
+        return add_cap(address, amountUSD, amountDAI);
+    } catch (error) {
+        log.info(TAG + 'add_cap error:', error);
+        throw error;
+    }
+}
 
-        //get cap table
-        let allCap = await database.getAllCapEntries()
-        log.info("allCap: ", allCap)
+export function remove(address: string, percent: number): Promise<CapEntry> {
+    try {
+        return remove_cap(address, percent);
+    } catch (error) {
+        log.info(TAG + 'remove_cap error:', error);
+        throw error;
+    }
+}
 
-        if(allCap.length > 0){
-            CAP_TABLE = allCap;
-        }else{
+function calculateValueByOwner(): { address: string; valueUSD: number; valueDAI: number }[] {
+    try {
+        const valueByOwner: { address: string; valueUSD: number; valueDAI: number }[] = [];
+        const totalLPTokens = getTotalLPTokens();
+
+        for (const entry of CAP_TABLE) {
+            const address = entry.address;
+            const lpTokens = entry.lpTokens;
+
+            const valueUSD = (lpTokens / totalLPTokens) * TOTAL_USD;
+            const valueDAI = (lpTokens / totalLPTokens) * TOTAL_DAI;
+
+            valueByOwner.push({
+                address: address,
+                valueUSD: valueUSD,
+                valueDAI: valueDAI,
+            });
+        }
+
+        return valueByOwner;
+    } catch (error) {
+        log.info(TAG + 'calculateValueByOwner error:', error);
+        throw error;
+    }
+}
+
+async function init_cap(): Promise<void> {
+    try {
+        CAP_TABLE = await database.getAllCapEntries();
+
+        if (CAP_TABLE.length === 0) {
             const totalLPTokens = calculateTotalLPTokens(TOTAL_USD, TOTAL_DAI);
-            log.info("totalLPTokens: ", totalLPTokens)
+            const ownershipPercentage = 100;
 
-            let ownershipPercentage = 1
-            CAP_TABLE.push({
+            const entry: CapEntry = {
+                id: 0,
                 address: ATM_OWNER,
                 lpTokens: totalLPTokens,
-                percentage: 100
-            });
-            database.addCapitalEntry(ATM_OWNER, totalLPTokens, 100)
+                percentage: ownershipPercentage,
+            };
+
+            CAP_TABLE.push(entry);
+            await database.addCapitalEntry(ATM_OWNER, totalLPTokens, ownershipPercentage);
         }
-
-        //create DB entry
-        log.info("CAP_TABLE: ", CAP_TABLE)
-    } catch (e) {
-        console.error(tag, "e: ", e);
-        throw e;
+    } catch (error) {
+        log.info(TAG + 'init_cap error:', error);
+        throw error;
     }
-};
-
-function calculateOwnershipPercentage(amountUSD, amountDAI, totalLPTokens) {
-    if (totalLPTokens === 0) {
-        return 100;
-    }
-
-    const lpTokens = (amountUSD / TOTAL_USD) + (amountDAI / TOTAL_DAI);
-    const ownershipPercentage = lpTokens / totalLPTokens;
-
-    return ownershipPercentage;
 }
 
-let add_cap = async function (address, amountUSD, amountDAI) {
-    let tag = TAG + " | add_cap | ";
+function add_cap(address: string, amountUSD: number, amountDAI: number): Promise<CapEntry> {
     try {
-        const totalLPTokensBefore = getTotalLPTokens();
+        const existingEntry = CAP_TABLE.find((entry) => entry.address === address);
 
-        TOTAL_USD += amountUSD;
-        TOTAL_DAI += amountDAI;
+        if (existingEntry) {
+            const totalLPTokensBefore = getTotalLPTokens();
+            TOTAL_USD += amountUSD;
+            TOTAL_DAI += amountDAI;
 
-        const totalLPTokensAfter = calculateTotalLPTokens(TOTAL_USD, TOTAL_DAI);
+            const totalLPTokensAfter = calculateTotalLPTokens(TOTAL_USD, TOTAL_DAI);
+            const lpTokensToMint = totalLPTokensAfter - totalLPTokensBefore;
 
-        const lpTokensToMint = totalLPTokensAfter - totalLPTokensBefore;
+            TOTAL_LP_TOKENS += lpTokensToMint;
 
-        TOTAL_LP_TOKENS += lpTokensToMint; // Update total LP tokens
-        let percentage = (lpTokensToMint / totalLPTokensAfter) * 100
-        let entry = {
-            address: address,
-            lpTokens: lpTokensToMint,
-            percentage
-        };
-        CAP_TABLE.push(entry);
-        database.addCapitalEntry(ATM_OWNER, lpTokensToMint, 100)
+            existingEntry.lpTokens += lpTokensToMint;
+            existingEntry.percentage = (existingEntry.lpTokens / totalLPTokensAfter) * 100;
 
-        //get cap table
-        let allCap = await database.getAllCapEntries()
-        log.info("allCap: ", allCap)
-        
-        //recalibrate percentages of pool
-        for(let i = 0; i < allCap.length; i++){
-            //new percentage
-            let percentage = (allCap[i].lpTokens / TOTAL_LP_TOKENS) * 100
-            allCap[i].percentage = percentage
-            //update db
-            database.updateCapitalEntry(allCap[i].address, percentage)
+            if (!existingEntry.lpTokens) throw Error("Invalid existingEntry.lpTokens");
+            if (!existingEntry.id) throw Error("Invalid existingEntry.id");
+
+            return database.updateCapitalEntry(existingEntry.id, existingEntry.lpTokens, existingEntry.percentage).then(() => existingEntry);
+        } else {
+            const totalLPTokensBefore = getTotalLPTokens();
+            TOTAL_USD += amountUSD;
+            TOTAL_DAI += amountDAI;
+
+            const totalLPTokensAfter = calculateTotalLPTokens(TOTAL_USD, TOTAL_DAI);
+            const lpTokensToMint = totalLPTokensAfter - totalLPTokensBefore;
+
+            TOTAL_LP_TOKENS += lpTokensToMint;
+
+            const percentage = (lpTokensToMint / totalLPTokensAfter) * 100;
+
+            const entry: CapEntry = {
+                id: 0,
+                address: address,
+                lpTokens: lpTokensToMint,
+                percentage: percentage,
+            };
+
+            CAP_TABLE.push(entry);
+            if (!lpTokensToMint) throw Error("Invalid lpTokensToMint");
+            if (!percentage) throw Error("Invalid percentage");
+
+            return database.addCapitalEntry(address, lpTokensToMint, percentage).then(() => entry);
         }
-        
-        return entry;
-    } catch (e) {
-        console.error(tag, "e: ", e);
-        throw e;
+    } catch (error) {
+        log.info(TAG + 'add_cap error:', error);
+        throw error;
     }
-};
+}
 
-const remove_cap = async (address, amountOut) => {
+function remove_cap(address: string, percent: number): Promise<CapEntry> {
     try {
-        log.info("TOTAL_USD: ", TOTAL_USD);
-        log.info("TOTAL_DAI: ", TOTAL_DAI);
-        console.log('remove_cap | start | address:', address, 'amountOut:', amountOut);
-
-        if (!address || amountOut === undefined || amountOut === null) {
-            throw new Error('Missing parameters: address or amountOut');
-        }
-
-        if (typeof amountOut !== 'number' || amountOut < 0 || amountOut > 100) {
-            throw new Error('Invalid parameter: amountOut should be a number between 0 and 100');
-        }
-
         const entry = CAP_TABLE.find(e => e.address === address);
 
         if (!entry) {
             throw new Error(`No entry found for address: ${address}`);
         }
 
-        console.log('remove_cap | entry found:', entry);
-
-        const lpTokensOut = (entry.lpTokens * amountOut) / 100;
-        console.log('remove_cap | lpTokensOut:', lpTokensOut);
+        const lpTokensOut = (entry.lpTokens * percent) / 100;
 
         if (lpTokensOut > entry.lpTokens) {
-            throw new Error(`Insufficient LP tokens: User does not have enough LP tokens to withdraw ${amountOut}%`);
+            throw new Error(`Insufficient LP tokens: User does not have enough LP tokens to withdraw ${percent}%`);
         }
 
-        const lpTokenValueUSD = TOTAL_USD / TOTAL_LP_TOKENS;
-        const lpTokenValueDAI = (TOTAL_DAI * (10 ** 18)) / TOTAL_LP_TOKENS;
-        console.log('remove_cap | lpTokenValueUSD:', lpTokenValueUSD);
-        console.log('remove_cap | lpTokenValueDAI:', lpTokenValueDAI);
-
-        const dispenseUSD = lpTokensOut * lpTokenValueUSD;
-        let dispenseDAI = (lpTokensOut * lpTokenValueDAI) / (10 ** 18);
-        dispenseDAI = Math.round(dispenseDAI * 100) / 100; // Round to 2 decimal places
-        console.log('remove_cap | dispenseUSD:', dispenseUSD);
-        console.log('remove_cap | dispenseDAI:', dispenseDAI);
-
-
-        // Ensure that the pool has enough USD and DAI to dispense
-        if (dispenseUSD > TOTAL_USD || dispenseDAI > TOTAL_DAI) {
-            throw new Error('Insufficient funds in the pool');
-        }
+        const totalLpTokensInPool = getTotalLPTokens();
 
         entry.lpTokens -= lpTokensOut;
-        console.log('remove_cap | new entry.lpTokens:', entry.lpTokens);
+        entry.percentage = (entry.lpTokens / totalLpTokensInPool) * 100;
 
-        TOTAL_LP_TOKENS -= lpTokensOut; // Update total LP tokens
-        console.log('remove_cap | new TOTAL_LP_TOKENS:', TOTAL_LP_TOKENS);
-
-        TOTAL_USD -= dispenseUSD;
-        TOTAL_DAI -= dispenseDAI;
-        console.log('remove_cap | new TOTAL_USD:', TOTAL_USD);
-        console.log('remove_cap | new TOTAL_DAI:', TOTAL_DAI);
-
-        const totalLpTokensInPool = CAP_TABLE.reduce((total, e) => total + e.lpTokens, 0);
-        if (totalLpTokensInPool === 0) {
-            entry.percentage = 0;
-        } else {
-            entry.percentage = (entry.lpTokens / totalLpTokensInPool) * 100;
-        }
-
-        console.log('remove_cap | new entry.percentage:', entry.percentage);
-
-        return {
-            ...entry,
-            dispenseUSD,
-            dispenseDAI,
-        };
-
-    } catch (e) {
-        console.error('remove_cap | e: ', e);
-        throw e;
+        return database.updateCapitalEntry(entry.id, entry.lpTokens, entry.percentage).then(() => entry);
+    } catch (error) {
+        log.info(TAG + 'remove_cap error:', error);
+        throw error;
     }
-};
-
-
-function getTotalLPTokens() {
-    let totalLPTokens = 0;
-    for (const entry of CAP_TABLE) {
-        totalLPTokens += entry.lpTokens;
-    }
-    return totalLPTokens;
 }
 
-function calculateTotalLPTokens(TOTAL_USD, TOTAL_DAI) {
-    const x = TOTAL_USD;
-    const y = TOTAL_DAI;
-    const k = x * y;
-    const totalLPTokens = Math.sqrt(k) * 10 ** 18;
+function getTotalLPTokens(): number {
+    try {
+        let totalLPTokens = 0;
+        for (const entry of CAP_TABLE) {
+            totalLPTokens += entry.lpTokens;
+        }
+        return totalLPTokens;
+    } catch (error) {
+        log.info(TAG + 'getTotalLPTokens error:', error);
+        throw error;
+    }
+}
 
-    return totalLPTokens || 0;
+function calculateTotalLPTokens(TOTAL_USD: number, TOTAL_DAI: number): number {
+    try {
+        const x = TOTAL_USD;
+        const y = TOTAL_DAI;
+        const k = x * y;
+        const totalLPTokens = Math.sqrt(k) * 10 ** 18;
+
+        return totalLPTokens || 0;
+    } catch (error) {
+        log.info(TAG + 'calculateTotalLPTokens error:', error);
+        throw error;
+    }
 }
